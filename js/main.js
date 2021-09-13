a2fefaec10ebcd4dfb8218f08e7d63758f88527f
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-13 11:15:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,130 @@
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+const icons = [
+  {
+    name: 'apple-alt',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'ice-cream',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'fish',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'lemon',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'hamburger',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'pizza-slice',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "food"
+  },
+  {
+    name: 'beer',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'glass-whiskey',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'wine-bottle',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'cocktail',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'coffee',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'glass-martini',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "beverage"
+  },
+  {
+    name: 'dragon',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'kiwi-bird',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'frog',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'hippo',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'otter',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+  {
+    name: 'horse',
+    family: 'fas',
+    prefix: 'fa-',
+    category: "animal"
+  },
+];
+
+const mainContent = document.getElementById('main-content')
+
+icons.forEach(
+  (icon) => {
+    const {name, family, prefix, category} = icon
+
+    mainContent.innerHTML += `
+      <div class="card">
+            <i class="${family} ${prefix}${name}"></i>
+          <div class="card-text">
+            <h4>${name}</h4>
+          </div>
+        </div>
+    `
+  }
+)

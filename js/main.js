3ea9fd135a95e19ce70f95679713f9ed261c1912
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-09-13 15:58:04 +0200

    add milestone 3

--- js/main.js
+++ js/main.js
@@ -5,6 +5,38 @@
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
 
+/**
+ * Functions
+ */
+
+/**
+ * dato l'array crea le card nell'html
+ */
+const printCards = (icons) => {
+  //reset iniziale
+  mainContent.innerHTML = ''
+
+  icons.forEach(
+    (icon) => {
+      const { name, family, prefix, color } = icon;
+
+      mainContent.innerHTML += `
+      <div class="card">
+            <i class="${family} ${prefix}${name}" style="color: ${color}"></i>
+          <div class="card-text">
+            <h5>${name}</h5>
+          </div>
+        </div>
+    `;
+    }
+  );
+};
+
+
+/**
+ * Variables
+ */
+
 const icons = [
   {
     name: 'apple-alt',
@@ -120,32 +152,62 @@ const colors = {
   'food': 'green',
   'beverage': 'blue',
   'animal': 'brown'
-}
+};
 
-const mainContent = document.getElementById('main-content')
+const mainContent = document.getElementById('main-content');
 
+/**
+ * Main program
+ */
+
+// aggiungo il colore delle icone tramite l'oggetto colors
 const coloredIcons = icons.map(
   (icon) => {
     return {
       ...icon,
       'color': colors[icon.category]
-    }
-  }
-)
-
-
-coloredIcons.forEach(
-  (icon) => {
-    const {name, family, prefix, color} = icon
-
-    mainContent.innerHTML += `
-      <div class="card">
-            <i class="${family} ${prefix}${name}" style="color: ${color}"></i>
-          <div class="card-text">
-            <h4>${name}</h4>
-          </div>
-        </div>
-    `
+    };
   }
 );
 
+
+// aggiungo le options alla select in base alle categorie
+//    estraggo le categorie dall'array
+const categories = [];
+coloredIcons.forEach(
+  (icon) => {
+    if (!categories.includes(icon.category)) {
+      categories.push(icon.category);
+    }
+  }
+);
+console.log(categories);
+
+//    creo le options
+const categoriesSelect = document.getElementById('categories');
+categories.forEach(
+  (category) => {
+    categoriesSelect.innerHTML += `<option value="${category}">${category}</option>`;
+  }
+);
+
+
+// stampo a video le cards
+printCards(coloredIcons);
+
+// al change della select filtro per categorie
+categoriesSelect.addEventListener('change',
+  () => {
+    // creo un array di oggetti filtrati
+    const filteredIcons = coloredIcons.filter(
+      (icon) => {
+        if (icon.category == categoriesSelect.value || categoriesSelect.value == '') {
+          return true;
+        }
+      }
+    );
+
+    // stampo a video le cards filtrate
+    printCards(filteredIcons);
+  }
+);
